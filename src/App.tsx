cf87e19762e3061ef2698a48e43b950b3a5e566c
Danny <daniel.mcanulty@maine.edu>
2022-10-11 19:04:15 -0400

    None

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -12,12 +15,14 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
             <img src="dog.jpg" alt="A picture of some dogs" />
             <ul>
                 <li>Both of these dogs appear to be staring at something</li>
                 <li>These dogs are both very cute!</li>
                 <li>The dogs are both rather small</li>
             </ul>
+
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
