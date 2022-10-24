27a5b2ccf59b22b9fd17bb2c20b9d50b1638d56d
Danny <daniel.mcanulty@maine.edu>
2022-10-24 12:52:02 -0400

    IDK

--- src/arrays.ts
+++ src/arrays.ts
@@ -119,5 +119,19 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    return [];
+    const neg = values.findIndex((unk: number): boolean => unk < 0);
+    const nArray = [...values];
+    if (neg === -1) {
+        const sum = values.reduce(
+            (total: number, num: number) => total + num,
+            0
+        );
+        nArray.push(sum);
+    } else {
+        const sum = values
+            .slice(0, neg)
+            .reduce((total: number, num: number) => total + num, 0);
+        nArray.splice(neg + 1, 0, sum);
+    }
+    return nArray;
 }
