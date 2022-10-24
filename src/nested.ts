3c725bf27d98be8404bc9e09edd34d30ed617ba2
Danny <daniel.mcanulty@maine.edu>
2022-10-24 14:45:44 -0400

    sumPoints func

--- src/nested.ts
+++ src/nested.ts
@@ -68,7 +68,11 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    let sum = 0;
+    for (let i = 0; i < questions.length; i++) {
+        sum = sum + questions[i].points;
+    }
+    return sum;
 }
 
 /***
