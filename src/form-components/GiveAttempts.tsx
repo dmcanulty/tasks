e2a27eba3ae1a9ef7f44450a3826a770aa80ec8d
Danny <daniel.mcanulty@maine.edu>
2022-11-20 09:53:48 -0500

    giveAttempts progress

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempt, setAttempt] = useState<number>(3);
+    const [request, setRequest] = useState<number>(0);
     return (
         <div>
             <h3>Give Attempts</h3>
