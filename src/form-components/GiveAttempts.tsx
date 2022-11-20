225e59fa7972e45ade4820b75f9c8e877d4b5467
Danny <daniel.mcanulty@maine.edu>
2022-11-20 10:13:40 -0500

    giveAttempts complete

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -10,34 +10,34 @@ export function GiveAttempts(): JSX.Element {
             <Form.Group>
                 <Form.Label>GiveAttempts</Form.Label>
                 <Form.Control
-                type = "number"
-                value = {request}
-                onChange = {(a) => { 
-                const b = parseInt(a.target.value);
-                if(!isNaN(b)){
-                    setRequest(b);
-                }
-            }}
-            />
-            <button
-                name = "use"
-                disabled ={attempt <= 0}
-                onClick={() => {
-                    setAttempt(attempt - 1)
-                }
-            }
-            >
-                use
-            </button>
-            <button
-             name = "gain"
-                onClick={() => {
-                    setAttempt(attempt + request)
-                }
-            }
-            ></button>
-
-
+                    type="number"
+                    value={request}
+                    onChange={(a) => {
+                        const b = parseInt(a.target.value);
+                        if (!isNaN(b)) {
+                            setRequest(b);
+                        }
+                    }}
+                />
+                <button
+                    name="use"
+                    disabled={attempt <= 0}
+                    onClick={() => {
+                        setAttempt(attempt - 1);
+                    }}
+                >
+                    use
+                </button>
+                <button
+                    name="gain"
+                    onClick={() => {
+                        setAttempt(attempt + request);
+                    }}
+                >
+                    gain
+                </button>
+                <div> Attempts Remaining: {attempt} </div>
+            </Form.Group>
         </div>
     );
 }
