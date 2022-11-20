8f598f2fde134e4a69a389212078dbb972b921e9
Danny <daniel.mcanulty@maine.edu>
2022-11-20 10:04:23 -0500

    use button done

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,11 +1,37 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { isArrayBindingPattern } from "typescript";
 
 export function GiveAttempts(): JSX.Element {
     const [attempt, setAttempt] = useState<number>(3);
     const [request, setRequest] = useState<number>(0);
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <Form.Group>
+                <Form.Label>GiveAttempts</Form.Label>
+                <Form.Control
+                type = "number"
+                value = {request}
+                onChange = {(a) => { 
+                const b = parseInt(a.target.value);
+                if(!isNaN(b)){
+                    setRequest(b);
+                }
+            }}
+            />
+            <button
+                name = "use"
+                disabled ={attempt <= 0}
+                onClick{() => {
+                    setAttempt(attempt - 1)
+                }
+            }
+            >
+                use
+            </button>
+            <button
+            
+
         </div>
     );
 }
