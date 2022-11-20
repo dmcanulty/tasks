9e8042efb03055eabf01a49c759e00c4234001a4
Danny <daniel.mcanulty@maine.edu>
2022-11-20 11:00:45 -0500

    EditMode complete

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -4,14 +4,41 @@ import { Form } from "react-bootstrap";
 export function EditMode(): JSX.Element {
     const [edit, setEdit] = useState<boolean>(false);
     const [name, setName] = useState<string>("Your Name");
-    const [student, setStudent] = useState<boolean>(false);
+    const [student, setStudent] = useState<boolean>(true);
     return (
         <div>
             <Form.Group>
                 <Form.Check
-                    type = "switch"
-                    
+                    type="switch"
+                    id="change"
+                    label="Edit"
+                    checked={edit}
+                    onChange={(a) => {
+                        setEdit(a.target.checked);
+                    }}
+                />
+                <Form.Check
+                    hidden={!edit}
+                    type="checkbox"
+                    id="student"
+                    label="is student"
+                    name="student"
+                    checked={student}
+                    onChange={(a) => {
+                        setStudent(a.target.checked);
+                    }}
+                />
+                <Form.Control
+                    hidden={!edit}
+                    value={name}
+                    onChange={(a) => {
+                        setName(a.target.value);
+                    }}
+                />
             </Form.Group>
+            <h3>
+                {name} is {student ? "" : "not"} a student
+            </h3>
         </div>
     );
 }
