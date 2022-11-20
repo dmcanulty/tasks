34cae46c649cdfa8a927033ca430ff866cb6557c
Danny <daniel.mcanulty@maine.edu>
2022-11-20 10:48:22 -0500

    progress on EditMode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [student, setStudent] = useState<boolean>(false);
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Group>
+                <Form.Check
+                    type = "switch"
+                    
+            </Form.Group>
         </div>
     );
 }
